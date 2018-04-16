0b16bcf7e0c51069be9f162cc05855a784ca4683
John Matthews <johnmatthews2634@gmail.com>
2018-04-16 16:01:21 -0700

    player toggle working

--- tictac/main.jsx
+++ tictac/main.jsx
@@ -2,6 +2,19 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-  
 
+  let playerToggledivs = document.getElementById('player-select');
+  let playerToggle = "X";
+  console.log(playerToggledivs);
+
+  playerToggledivs.addEventListener("click",(e) => {
+    e.target.style.background = "green";
+    playerToggledivs = document.getElementById(playerToggle);
+    playerToggledivs.style.background = "white";
+    if (playerToggle === "X") {
+      playerToggle = "O";
+    } else {
+      playerToggle = "X";
+    }
+  });
 });
